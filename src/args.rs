e551c2889b2f434819e425015009ca6f59cd32cc
EnormousCloud <enormous@webcerebrium.com>
2021-09-09 09:14:28 +0300

    chainstate: better telemetry

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,5 @@
 use structopt::StructOpt;
+use tracing_subscriber::prelude::*;
 
 #[derive(Debug, StructOpt, Clone)]
 #[structopt(
@@ -6,19 +7,29 @@ use structopt::StructOpt;
     about = "Enormous Cloud chainstate API server"
 )]
 pub struct Args {
-    #[structopt(
-        long,
-        default_value = "http://127.0.0.1:8545",
-        env = "RPC_ENDPOINT"
-    )]
-    pub eth1: String,
-    #[structopt(long, default_value = "./dist", env = "STATIC_DIR")]
-    pub static_dir: String,
+    #[structopt(long, default_value = "http://127.0.0.1:8545", env = "RPC_ENDPOINT")]
+    pub network: String,
     #[structopt(short, long, default_value = "0.0.0.0:8000", env = "LISTEN")]
     pub addr: String,
 }
 
 pub fn parse() -> anyhow::Result<Args> {
+    let log_level: String = std::env::var("LOG_LEVEL").unwrap_or("info".to_owned());
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        // .without_time()
+        // .with_ansi(false)
+        // .with_level(false)
+        // .with_target(false)
+    ;
+    let filter_layer = tracing_subscriber::EnvFilter::try_from_default_env()
+        .or_else(|_| tracing_subscriber::EnvFilter::try_new(&log_level))
+        .unwrap();
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
+
     let res = Args::from_args();
     tracing::info!("{:?}", res);
     // todo: check static dir exists
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,11 @@ pub mod telemetry;
 
 #[derive(Clone)]
 pub struct State {
-    pub static_dir: String,
     pub eth1: String,
 }
 
-// use std::path::{Path, PathBuf};
-// use std::{ffi::OsStr, io};
-// use tide::{Body, Middleware, Next, Request, Response, StatusCode};
-
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::new("info"))
-        .init();
     let args = match args::parse() {
         Ok(x) => x,
         Err(e) => {
@@ -25,8 +17,7 @@ async fn main() -> tide::Result<()> {
     };
 
     let state = State {
-        static_dir: args.static_dir.clone(),
-        eth1: args.eth1.clone(),
+        eth1: args.network.clone(),
     };
     let mut app = tide::with_state(state);
     app.with(telemetry::TraceMiddleware::new());
